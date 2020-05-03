bcaf538c4b427741c0943ee7ac616335dae80950
Danila Lebedev <daniilvlebedev@gmail.com>
2020-05-03 20:51:07 +0300

    Task5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void test() throws Exception {
         BinaryTree<Integer> binaryTree = new BinaryTree<>(s -> Integer.parseInt(s));
-
         binaryTree.fromBracketNotation("20(15(11,19),25(21,29))");
         int min = binaryTree.min();
         System.out.println(min);
